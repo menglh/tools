4dc8d2a20198e268887dfc6d0afc7f22de2df5f1
meng <lingheng.meng@henganpros.com>
2021-04-20 17:06:27 +0800

    edit

--- mail/mail.go
+++ mail/mail.go
@@ -2,48 +2,28 @@ package mail
 
 import (
 	"gopkg.in/gomail.v2"
-	"strconv"
 )
 
 type Mail struct {
-	Options
-	MailInfo
-}
-
-type Options struct {
 	User string
 	Pass string
 	Host string
-	Port string
+	Port int
 }
 
-type MailInfo struct {
+func NewMail(data Mail) *Mail {
+	return &Mail{User: data.User, Pass: data.Pass, Host: data.Host, Port: data.Port}
 }
 
-func NewMail() *Mail {
-	return &Mail{}
-}
-
-func Send(mailTo []string, subject string, body string) error {
-	//定义邮箱服务器连接信息，如果是阿里邮箱 pass填密码，qq邮箱填授权码
-	mailConn := map[string]string{
-		"user": "mlh_210@sina.com",
-		"pass": "d0c10aa1818dff22",
-		"host": "smtp.sina.com",
-		"port": "465",
-	}
-
-	port, _ := strconv.Atoi(mailConn["port"]) //转换端口类型为int
-
+func (mail *Mail) Send(mailTo []string, subject string, body string) error {
 	m := gomail.NewMessage()
-	m.SetHeader("From", "XD Game"+"<"+mailConn["user"]+">") //这种方式可以添加别名，即“XD Game”， 也可以直接用<code>m.SetHeader("From",mailConn["user"])</code> 读者可以自行实验下效果
-	m.SetHeader("To", mailTo...)                            //发送给多个用户
-	m.SetHeader("Subject", subject)                         //设置邮件主题
-	m.SetBody("text/html", body)                            //设置邮件正文
+	m.SetHeader("From", "<"+mail.User+">")
+	m.SetHeader("To", mailTo...)    //发送给多个用户
+	m.SetHeader("Subject", subject) //设置邮件主题
+	m.SetBody("text/html", body)    //设置邮件正文
 
-	d := gomail.NewDialer(mailConn["host"], port, mailConn["user"], mailConn["pass"])
+	d := gomail.NewDialer(mail.Host, mail.Port, mail.User, mail.Pass)
 
 	err := d.DialAndSend(m)
 	return err
-
 }
