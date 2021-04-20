504b9e8af3a628b78b3004fb76fb7c981becf259
meng <lingheng.meng@henganpros.com>
2021-04-20 17:23:10 +0800

    edit

--- mail/mail.go
+++ mail/mail.go
@@ -4,6 +4,19 @@ import (
 	"gopkg.in/gomail.v2"
 )
 
+/**
+option := mail.Mail{
+		User: "mlh_210@sina.com",
+		Pass: "d0c10aa1818dff22",
+		Host: "smtp.sina.com",
+		Port: 465,
+	}
+	if err := mail.NewMail(option).Send([]string{"mlh_210@163.com"},"测试的","测试的");err != nil{
+		fmt.Errorf("发送邮件错误%s",err.Error())
+		return
+	}
+	fmt.Println("success...")
+*/
 type Mail struct {
 	User string
 	Pass string
